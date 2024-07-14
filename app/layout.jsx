32f52f92da1200b35c1c256a50ab22898b40a99c
Marcin <marcinchodkowski004@wp.pl>
2024-07-14 16:43:57 +0200

    added meta icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,12 @@ import '@styles/globals.css';
 export const metadata = {
 	title: 'Promptopia',
 	description: 'Discover & Share AI Prompts',
+	icons: {
+		icon: ['/favicon.ico?v-4'],
+		apple:['/apple-touch-icon.png?v=4'],
+		shortcut:['/apple-touch-icon.png']
+	},
+	manifest: '/site.webmanifest',
 };
 
 const RootLayout = ({ children }) => {
